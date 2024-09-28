bbb4c5adc994d50b87eff8f1d87091e678f5934c
fidrickbenjamin <tacticaltrends.dm@gmail.com>
2024-09-28 07:49:21 -0400

    cart Checkout settings

--- frontend/src/components/Cart/Shipping.jsx
+++ frontend/src/components/Cart/Shipping.jsx
@@ -1,142 +1,135 @@
 import React, { useEffect, useState } from "react";
-import {  countries  } from "countries-list";
+import { countries } from "countries-list";
 import { useDispatch, useSelector } from "react-redux";
-import { saveShippingInfo } from "../../redux/features/cartSlice";
 import { useNavigate } from "react-router-dom";
 import MetaData from "../layout/MetaData";
 import CheckoutSteps from "./CheckoutSteps";
 
 const Shipping = () => {
-    
-    const countriesList = Object.values(countries);
+  const countriesList = Object.values(countries);
+  const navigate = useNavigate();
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth); // Access the user from the Redux store
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [phoneNo, setPhoneNo] = useState("");
+  const [country, setCountry] = useState("");
 
-    const [address, setAddress] = useState("");
-    const [city, setCity] = useState("");
-    const [zipCode, setZipCode] = useState("");
-    const [phoneNo, setPhoneNo] = useState("");
-    const [country, setCountry] = useState("");
+  useEffect(() => {
+    if (user && user.shippingInfo) {
+      // Pre-fill the form with user shipping info
+      setAddress(user.shippingInfo.address || "");
+      setCity(user.shippingInfo.city || "");
+      setZipCode(user.shippingInfo.zipCode || "");
+      setPhoneNo(user.shippingInfo.phoneNo || "");
+      setCountry(user.shippingInfo.country || "");
+    }
+  }, [user]);
 
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-    const { shippingInfo } = useSelector((state ) => state.cart);
+    // We are not dispatching any action to update the user shipping info in Redux
+    // This allows users to edit the form but doesn't save it to the Redux store or backend
 
-    useEffect(() => {
-        if(shippingInfo) {
-            setAddress(shippingInfo?.address);
-            setCity(shippingInfo?.city);
-            setZipCode(shippingInfo?.zipCode);
-            setPhoneNo(shippingInfo?.phoneNo);
-            setCountry(shippingInfo?.country);
-        }
-    }, [shippingInfo] );
+    // Optionally, navigate to confirm order page without updating user shipping info
+    navigate("/confirm_order");
+  };
 
-            const submitHandler = (e) => {
-                e.preventDefault()
+  return (
+    <>
+      <MetaData title={"Shipping Info"} />
+      <CheckoutSteps shipping />
 
-                dispatch(saveShippingInfo({ address, city, phoneNo, zipCode, country }));
-                navigate("/confirm_order");
-            };
+      <div className="row wrapper mb-5">
+        <div className="col-10 col-lg-5">
+          <form className="shadow rounded bg-body" onSubmit={submitHandler}>
+            <h2 className="mb-4">Shipping Info</h2>
 
-    
-    return (
-          
-     <>
-     <MetaData title={"Shipping Info"} />
+            <div className="mb-3">
+              <label htmlFor="address_field" className="form-label">
+                Address
+              </label>
+              <input
+                type="text"
+                id="address_field"
+                className="form-control"
+                name="address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
 
-     <CheckoutSteps shipping /> 
+            <div className="mb-3">
+              <label htmlFor="city_field" className="form-label">
+                City
+              </label>
+              <input
+                type="text"
+                id="city_field"
+                className="form-control"
+                name="city"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </div>
 
-     <div className="row wrapper mb-5">
-      <div className="col-10 col-lg-5">
-        <form
-          className="shadow rounded bg-body"
-          onSubmit={submitHandler}
-        >
-          <h2 className="mb-4">Shipping Info</h2>
-          <div className="mb-3">
-            <label htmlFor="address_field" className="form-label">Address</label>
-            <input
-              type="text"
-              id="address_field"
-              className="form-control"
-              name="address"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              required
-            />
-          </div>
+            <div className="mb-3">
+              <label htmlFor="phone_field" className="form-label">
+                Phone No
+              </label>
+              <input
+                type="tel"
+                id="phone_field"
+                className="form-control"
+                name="phoneNo"
+                value={phoneNo}
+                onChange={(e) => setPhoneNo(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-3">
-            <label htmlFor="city_field" className="form-label">City</label>
-            <input
-              type="text"
-              id="city_field"
-              className="form-control"
-              name="city"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              required
-            />
-          </div>
+            <div className="mb-3">
+              <label htmlFor="zip_code_field" className="form-label">
+                Zip Code
+              </label>
+              <input
+                type="number"
+                id="zip_code_field"
+                className="form-control"
+                name="zipCode"
+                value={zipCode}
+                onChange={(e) => setZipCode(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-3">
-            <label htmlFor="phone_field" className="form-label">Phone No</label>
-            <input
-              type="tel"
-              id="phone_field"
-              className="form-control"
-              name="phoneNo"
-              value={phoneNo}
-              onChange={(e) => setPhoneNo(e.target.value)}
-              required
-            />
-          </div>
+            <div className="mb-3">
+              <label htmlFor="country_field" className="form-label">
+                Country
+              </label>
+              <select
+                id="country_field"
+                className="form-select"
+                name="country"
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+              >
+                {countriesList.map((country) => (
+                  <option key={country.name} value={country.name}>
+                    {country.name}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-          <div className="mb-3">
-            <label htmlFor="zip_code_field" className="form-label"
-              > Zip Code</label>
-            <input
-              type="number"
-              id="zip_code_field"
-              className="form-control"
-              name="postalCode"
-              value={zipCode}
-              onChange={(e) => setZipCode(e.target.value)}
-              required
-            />
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="country_field" className="form-label">Country</label>
-            <select
-              id="country_field"
-              className="form-select"
-              name="country"
-              value={country}
-              onChange={(e) => setCountry(e.target.value)}
-              required
-            >
-               {countriesList?.map((country) => (
-                <option key={country?.name} value={country?.name}>{country?.name}</option>
-               ))}
-              
-              
-               
-            </select>
-          </div>
-
-          <button id="shipping_btn" type="submit" className="btn w-100 py-2">
-            CONTINUE
-          </button>
-        </form>
+            <button id="shipping_btn" type="submit" className="btn w-100 py-2">
+              CONTINUE
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
-     
-     
-     </>
-     
-    );
+    </>
+  );
 };
 
 export default Shipping;
