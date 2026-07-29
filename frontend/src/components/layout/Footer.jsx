d44a61387dd8ddd5a165fcd7d05a4a348d9a1cf6
Fidrick Benjamin <fidrick1@gmail.com>
2026-07-28 21:47:40 -0400

    Tactical Trends Market Place Phase V4.9a

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
        <footer className="py-1 pt-5">
   <p className="text-center mt-1 fw-bold">
-    Tactical Trends Marketplace - {new Date().getFullYear()} |  Developer - Fidrick Benjamin | all Rights Reserved
+    Tactical Trends Market Place - {new Date().getFullYear()} |  Developer - Fidrick Benjamin | all Rights Reserved
   </p>
 </footer>
   
